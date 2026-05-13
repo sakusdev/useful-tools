48f95c932a826612bd497a904a05ecee6310e671
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-05-13 21:11:18 +0900

    Register service worker for PWA

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/globals.css'
+import { registerSW } from '@/pwa/registerSW'
+
+registerSW()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
